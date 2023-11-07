2f2bff89ee85674bcfaf4e810c4fb64e44cf062a
Prasann0009 <130238044+Prasann0009@users.noreply.github.com>
2023-11-07 21:32:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // complete the given function
 
 function palindrome(str){
-
+ s = s.replace(/[\W_]/g, '').toLowerCase();
+  return s === s.split('').reverse().join('');
 }
 module.exports = palindrome
+
